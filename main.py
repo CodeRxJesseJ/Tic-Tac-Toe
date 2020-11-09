056038737b594bc45f00ec7aeb1bbdcf2412ae2d
Jessej Samuel <jessejnathanael.s2020@vitstudent.ac.in>
2020-11-10 00:34:45 +0530

    Improved draw cases and game-over parts.

--- main.py
+++ main.py
@@ -9,5 +9,22 @@ update(game_board)
 while game_won(game_board) == False:
     update(game_board)
     if game_won(game_board) == True:
-        print("The game's over!")
-        break
+        if game_board.count("O") > game_board.count("X"):
+            print("O has won the game!")
+        else:
+            print("X has won the game!")
+    elif game_won(game_board) == False:
+        if (game_board.count("X") == 5 and game_board.count("O") == 4) or (game_board.count("X") == 4 and game_board.count("O") == 5):
+            print("Oh no! The game's a draw!")
+            replay = input("Wanna play again? (Y/N): ")
+            while replay != "Y" or "N":
+                if replay == "Y":
+                    print("Run the program agin then xD")
+                    break
+                elif replay == "N":
+                    print("Take care, goodbye!")
+                    break
+
+print("NOTE: If you are getting an error, then maybe you might have not followed the rules properly!")
+print("Please take time to visit https://en.wikipedia.org/wiki/Tic-tac-toe")
+print("Thank you for playing!")
